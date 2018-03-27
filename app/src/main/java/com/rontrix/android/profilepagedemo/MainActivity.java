a2ed5117cd7fafb0036b0e8729b8b8bae16adae8
Ron <rondaulagupu8@gmail.com>
2018-03-27 20:55:48 +0530

    Initial commit

---
+++ app/src/main/java/com/rontrix/android/profilepagedemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rontrix.android.profilepagedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
